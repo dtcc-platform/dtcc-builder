7f73e58e666ff4b9d0b2c76cfa4f1300d2fced7d
Anders Logg <anders@logg.org>
2019-03-11 21:45:54 +0100

    Add coordinate transformation using Proj.4
    Former-commit-id: 30194d9d254eb2f5f44d10179082afc6552ab11f

---
+++ include/CoordinateSystem.h
@@ -0,0 +1,39 @@
+// Coordinate system transformation.
+// Copyright (C) 2019 Anders Logg.
+
+#ifndef VC_COORDINATE_SYSTEM_H
+#define VC_COORDINATE_SYSTEM_H
+
+#include <proj.h>
+#include <sstream>
+
+#include "Point.h"
+
+namespace VirtualCity
+{
+
+class CoordinateSystem
+{
+public:
+
+    // Transform point from between coordinate systems U and V
+    static Point2D Transform(const Point2D& p, std::string U, std::string V)
+    {
+        // Create projection string
+        std::stringstream s;
+        s << "cs2cs +init=" << U << " +to +init=" << V;
+
+        // Create projection
+        PJ* P = proj_create (PJ_DEFAULT_CTX, s.str());
+
+        // Transform coordinate
+        PJ_COORD a = proj_coord (proj_torad(p.x), proj_torad(p.y), 0, 0);
+        PJ_COORD b = proj_trans(P, PJ_FWD, a);
+
+        // Clean up
+        proj_destroy(P);
+    }
+
+};
+
+}
