a473479a7df6e133b6eb91d47984950db590f3c2
Anders Logg <anders@logg.org>
2019-05-13 14:57:47 +0200

    Add demo directory for testing meshing pipeline
    Former-commit-id: ba5e2f88fa07240d0216156b60dc8f39744228f5

---
+++ demo/demo.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+# Demo of the mesh generation pipeline, including
+#
+# 1. Height map generation (vc-generate-heightmap)
+# 2. City model generation (vc-generate-citymodel)
+# 3. Volume mesh generation (vc-generate-mesh)
+
+# Generate height map
+#../vc-generate-heightmap/build/vc-generate-heightmap \
+#  ../data/*.las Parameters.json
+
+# Generate city model
+../vc-generate-citymodel/build/vc-generate-citymodel \
+  ../data/PropertyMap.shp HeightMap.json Parameters.json
+
+# Generate mesh
+../vc-generate-mesh/build/vc-generate-mesh \
+  CityModel.json HeightMap.json Parameters.json
