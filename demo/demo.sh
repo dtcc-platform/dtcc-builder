2d986967aa06cd957b3fe852d4fbb9e620b96d11
Anders Logg <anders@logg.org>
2019-05-20 18:12:04 +0200

    Initial work on VCCore Docker image
    Former-commit-id: 306ef0a6ec6d7f04cadc07798e3becb64c7c0b09

--- demo/demo.sh
+++ demo/demo.sh
@@ -2,18 +2,15 @@
 #
 # Demo of the mesh generation pipeline, including
 #
-# 1. Height map generation (vc-generate-heightmap)
-# 2. City model generation (vc-generate-citymodel)
-# 3. Volume mesh generation (vc-generate-mesh)
+# 1. Height map generation:  vc-generate-heightmap
+# 2. City model generation:  vc-generate-citymodel
+# 3. Volume mesh generation: vc-generate-mesh
 
 # Generate height map
-#../vc-generate-heightmap/build/vc-generate-heightmap \
-#  ../data/*.las Parameters.json
+../vc-generate-heightmap/build/vc-generate-heightmap Parameters.json
 
 # Generate city model
-#../vc-generate-citymodel/build/vc-generate-citymodel \
-#  ../data/PropertyMap.shp HeightMap.json Parameters.json
+../vc-generate-citymodel/build/vc-generate-citymodel Parameters.json
 
 # Generate mesh
-../vc-generate-mesh/build/vc-generate-mesh \
-  CityModel.json HeightMap.json Parameters.json
+../vc-generate-mesh/build/vc-generate-mesh Parameters.json
