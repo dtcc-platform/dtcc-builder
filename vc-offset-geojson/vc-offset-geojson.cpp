a9d9348f11ebe1b55ca7c8159bd68824469ba679
vbassn <vasilis@fcc.chalmers.se>
2019-06-03 00:04:04 +0200

    Fixed some bugs in offset. Still lots of debug messages printed.
    Former-commit-id: 37be152755fb1b7950b4472038c0e63d0579bea2

--- vc-offset-geojson/vc-offset-geojson.cpp
+++ vc-offset-geojson/vc-offset-geojson.cpp
@@ -1,7 +1,7 @@
 // VirtualCity@Chalmers: vc-offset-geojson
 // Anders Logg 2019
 // Vasilis Naserentin 2019
-
+// Offset in EPSG:3006 and in meters used in UE -148000 -6398600
 #include <string>
 #include <iostream>
 #include "JSON.h"
@@ -40,34 +40,48 @@ int main(int argc, char* argv[])
 	}
 	nlohmann::json j;
 	in >> j;
-	double tempx, tempy;
-	int offsetxx = x;
-	int offsetyy = y;
+	double curx, cury;
+	int offsetx = x;
+	int offsety = y;
 	std::cout << "JSON stuff" << std::endl;
-	std::cout << j["type"]<<std::endl;
-	std::cout << j["features"][0] << std::endl;
-	std::cout << j["features"].size()<< std::endl;
+	std::cout << "JSON has "<<j["features"].size()<<" of type "<<j["features"][0]["geometry"]["type"] << std::endl;
+
 	//std::cout << j["features"][0]["geometry"] << std::endl;
 	//show whole polygon, this returns [x,y] and change the coordinates
+	std::cout<<"Offsetting"<<std::endl;
 	for (int k = 0; k < j["features"].size(); k++)
 	{
-		for (int i = 0; i < j["features"][k]["geometry"]["coordinates"][0][0].size(); i++)
+		for (int i = 0; i < j["features"][k]["geometry"]["coordinates"][0].size(); i++)
 		{
-			//std::cout << j["features"][k]["geometry"]["coordinates"][0][0][i] << std::endl;
-			tempx = j["features"][k]["geometry"]["coordinates"][0][0][i][0];
-			tempy = j["features"][k]["geometry"]["coordinates"][0][0][i][1];
-
-			j["features"][k]["geometry"]["coordinates"][0][0][i][0] = tempx + offsetxx;
-			j["features"][k]["geometry"]["coordinates"][0][0][i][1] = tempy + offsetyy;
+			//tempx = j["features"][k]["geometry"]["coordinates"][0][0][i][0];
+			//tempy = j["features"][k]["geometry"]["coordinates"][0][0][i][1];
+			//std::cout<< j["features"][k]["geometry"]["coordinates"][0][i][0]<<std::endl;
+			//std::cout<< j["features"][k]["geometry"]["coordinates"][0][i][1]<<std::endl;
+			std::cout<<"Feature "<<k<<" point " <<i<<" out of "<<j["features"][k]["geometry"]["coordinates"][0].size()<<" has x "
+			<<j["features"][k]["geometry"]["coordinates"][0][i][0]<<"and y "<<j["features"][k]["geometry"]["coordinates"][0][i][1]<<std::endl;
+	                double originalx=j["features"][k]["geometry"]["coordinates"][0][i][0];
+			double originaly=j["features"][k]["geometry"]["coordinates"][0][i][1];
+			double newx=originalx+offsetx;
+			double newy=originaly+offsety;
+			j["features"][k]["geometry"]["coordinates"][0][i][0]=newx;
+			j["features"][k]["geometry"]["coordinates"][0][i][1]=newy;
+ 			std::cout<<"Feature "<<k<<" point " <<i<<" out of "<<j["features"][k]["geometry"]["coordinates"][0].size()<<" has x "
+                        <<j["features"][k]["geometry"]["coordinates"][0][i][0]<<"and y "<<j["features"][k]["geometry"]["coordinates"][0][i][1]<<std::endl;			
+			double validateoffsetx=newx-originalx;
+			double validateoffsety=newy-originaly;
+			std::cout<<"Offsetx is "<<validateoffsetx<<std::endl;
+   			std::cout<<"Offsety is "<<validateoffsety<<std::endl;
+			//j["features"][k]["geometry"]["coordinates"][0][0][i][0] = tempx + offsetxx;
+			//j["features"][k]["geometry"]["coordinates"][0][0][i][1] = tempy + offsetyy;
 
 		}
 		
-			std::cout << j["features"][k]["geometry"]["coordinates"][0][0] << std::endl;
+			//std::cout << j["features"][k]["geometry"]["coordinates"][0][0] << std::endl;
 
 
 		
 	}
-	
+	std::cout<<"Offset complete"<<std::endl;	
 	
 	std::cout << j["features"][0]["geometry"]["coordinates"][0][0][0].size() << std::endl;//first x
 	std::cout << j["features"][0]["geometry"]["coordinates"][0][0].size() << std::endl;//first x
