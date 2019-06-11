d9ec14bbcf1e28abe21e046e1c83f88ced0309f1
vbassn <vasilis@fcc.chalmers.se>
2019-06-11 13:13:14 +0200

    Added netcdf libs and new offset. Still need proper naming and clang format.
    Former-commit-id: f8306430852ade5f306a908044fd668cece01165

---
+++ cmake/FindCDF.cmake
@@ -0,0 +1,20 @@
+# Find the PROJ4 library and set the following standard variables:
+#
+# PROJ4_INCLUDE_DIRS - header location
+# PROJ4_LIBRARIES    - library to link against
+# PROJ4_FOUND        - true if library was found
+
+include(FindPackageHandleStandardArgs)
+
+find_path(CDF_INCLUDE_DIRS
+          NAMES netcdf.h
+          PATHS /usr/local/include)
+
+find_library(CDF_LIBRARIES
+             NAMES netcdf_c++4
+             PATHS /usr/local/lib)
+
+find_package_handle_standard_args(CDF
+                                  DEFAULT_MSG
+                                  CDF_INCLUDE_DIRS
+                                  CDF_LIBRARIES)
--- vc-offset-geojson/vc-offset-geojson.cpp
+++ vc-offset-geojson/vc-offset-geojson.cpp
@@ -2,93 +2,93 @@
 // Anders Logg 2019
 // Vasilis Naserentin 2019
 // Offset in EPSG:3006 used in UE -148000 -6398600 (meters)
-#include <string>
-#include <iostream>
 #include "JSON.h"
+#include <iostream>
+#include <string>
+#include "GeoJSON.h"
+#include <netcdf>
+using namespace VirtualCity;
 
-void Help()
-{
-    std::cerr << "Usage: vc-offset-geojson filein.geojson fileout.geojson x y " << std::endl;
+void Help() {
+  std::cerr << "Usage: vc-offset-geojson filein.geojson fileout.geojson x y "
+            << std::endl;
 }
 
-int main(int argc, char* argv[])
-{
-    // Check command-line arguments
-    if (argc != 5)
-    {
-      Help();
-      return 1;
+/*
+int detectmaxlevel(const nlohmann::json &ijson) {
+  int maxlevel = 1; 
+  int curlevel = 0;
+  while (maxlevel >= curlevel) {
+    std::cout << "Trying for curlevel " << curlevel << std::endl;
+    for (int k = 0; k < ijson["features"].size(); k++) {
+      if (ijson["features"][k]["geometry"]["coordinates"][curlevel].size() >
+          0) {
+        std::cout
+            << "Found "
+            << ijson["features"][k]["geometry"]["coordinates"][curlevel].size()
+            << std::endl;
+        std::cout << "curlevel " << curlevel << " is valid" << std::endl;
+
+        maxlevel++;
+        curlevel++;
+        std::cout << "New curlevel is " << curlevel << " and new maxlevel is "
+                  << maxlevel << std::endl;
+        break;
+      }
+      curlevel++;
     }
-	int x, y;
-	try
-	{
-	x = std::stoi(argv[3]);
-	y = std::stoi(argv[4]);
-	}
-	catch (...)
-	{
-	std::cout<<"Error; x and y must be integers."<<std::endl;
-	return 1;
-	}
-//	y = std::stoi(argv[4]);
-	std::string fileout = argv[2];
-	std::string filein = argv[1];
-	std::ifstream in(filein); 
-	if (in.fail()) {
-		std::cout << "Input file issue; check input file exists." << std::endl;
-		return 1;
-	}
-	nlohmann::json j;
-	in >> j;
-	double curx, cury;
-	int offsetx = x;
-	int offsety = y;
-	std::cout << "JSON stuff" << std::endl;
-	std::cout << "JSON has "<<j["features"].size()<<" of type "<<j["features"][0]["geometry"]["type"] << std::endl;
+  }
+  return maxlevel;
+}
+*/
+int main(int argc, char *argv[]) {
+  // Check command-line arguments
+  if (argc != 5) {
+    Help();
+    return 1;
+  }
+  int x, y;
+  try {
+    x = std::stod(argv[3]);
+    y = std::stod(argv[4]);
+  } catch (...) {
+    std::cout << "Error; x and y must be integers." << std::endl;
+    return 1;
+  }
+  std::string fileout = argv[2];
+  std::string filein = argv[1];
+  std::ifstream in(filein);
+  if (in.fail()) {
+    std::cout << "Input file issue; check input file exists." << std::endl;
+    return 1;
+  }
+  nlohmann::json json, jsoni;
+  in >> json;
+  jsoni = json;
+  double offsetx = x;
+  double offsety = y;
+  std::cout << "JSON stuff" << std::endl;
+  int maxlevel = GeoJSON::detectmaxlevel(json);
+  std::cout << "Max level found is " << maxlevel << std::endl;
+  std::cout << "Offsetting" << std::endl;
 
-	//std::cout << j["features"][0]["geometry"] << std::endl;
-	//show whole polygon, this returns [x,y] and change the coordinates
-	std::cout<<"Offsetting"<<std::endl;
-	for (int k = 0; k < j["features"].size(); k++)
-	{
-		for (int i = 0; i < j["features"][k]["geometry"]["coordinates"][0].size(); i++)
-		{
-			//tempx = j["features"][k]["geometry"]["coordinates"][0][0][i][0];
-			//tempy = j["features"][k]["geometry"]["coordinates"][0][0][i][1];
-			//std::cout<< j["features"][k]["geometry"]["coordinates"][0][i][0]<<std::endl;
-			//std::cout<< j["features"][k]["geometry"]["coordinates"][0][i][1]<<std::endl;
-			std::cout<<"Feature "<<k<<" point " <<i<<" out of "<<j["features"][k]["geometry"]["coordinates"][0].size()<<" has x "
-			<<j["features"][k]["geometry"]["coordinates"][0][i][0]<<"and y "<<j["features"][k]["geometry"]["coordinates"][0][i][1]<<std::endl;
-	                double originalx=j["features"][k]["geometry"]["coordinates"][0][i][0];
-			double originaly=j["features"][k]["geometry"]["coordinates"][0][i][1];
-			double newx=originalx+offsetx;
-			double newy=originaly+offsety;
-			j["features"][k]["geometry"]["coordinates"][0][i][0]=newx;
-			j["features"][k]["geometry"]["coordinates"][0][i][1]=newy;
- 			std::cout<<"Feature "<<k<<" point " <<i<<" out of "<<j["features"][k]["geometry"]["coordinates"][0].size()<<" has x "
-                        <<j["features"][k]["geometry"]["coordinates"][0][i][0]<<"and y "<<j["features"][k]["geometry"]["coordinates"][0][i][1]<<std::endl;			
-			double validateoffsetx=newx-originalx;
-			double validateoffsety=newy-originaly;
-			std::cout<<"Offsetx is "<<validateoffsetx<<std::endl;
-   			std::cout<<"Offsety is "<<validateoffsety<<std::endl;
-			//j["features"][k]["geometry"]["coordinates"][0][0][i][0] = tempx + offsetxx;
-			//j["features"][k]["geometry"]["coordinates"][0][0][i][1] = tempy + offsetyy;
-
-		}
-		
-			//std::cout << j["features"][k]["geometry"]["coordinates"][0][0] << std::endl;
-
-
-		
-	}
-	std::cout<<"Offset complete"<<std::endl;	
-	
-	std::cout << j["features"][0]["geometry"]["coordinates"][0][0][0].size() << std::endl;//first x
-	std::cout << j["features"][0]["geometry"]["coordinates"][0][0].size() << std::endl;//first x
-	//std::cout << j["features"][0]["geometry"]["coordinates"][0].size() << std::endl;//first x
-	std::ofstream o(fileout);
-	//o << std::setw(4) << j << std::endl;
-	o << j << std::endl;
-	
-	return 0;
+  for (int i = 0; i < maxlevel; i++) {
+    for (int k = 0; k < json["features"].size(); k++) {
+      for (int j = 0;
+           j < json["features"][k]["geometry"]["coordinates"][i].size(); j++) {
+        std::cout << i << " "
+                  << json["features"][k]["geometry"]["coordinates"][i][j][0]
+                  << std::endl;
+        double oldx = json["features"][k]["geometry"]["coordinates"][i][j][0];
+        double oldy = json["features"][k]["geometry"]["coordinates"][i][j][1];
+        double newx = oldx + offsetx;
+        double newy = oldy + offsety;
+        jsoni["features"][k]["geometry"]["coordinates"][i][j][0] = newx;
+        jsoni["features"][k]["geometry"]["coordinates"][i][j][1] = newy;
+      }
+    }
+  } 
+  std::ofstream o(fileout);
+  o << jsoni << std::endl;
+  return 0;
 }
