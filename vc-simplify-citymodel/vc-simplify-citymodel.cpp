7623e93ce777e7176ca3b2a6103f4a7d1b97e8da
Anders Logg <anders@logg.org>
2019-07-09 14:00:54 +0200

    Add missing files
    Former-commit-id: 48b37b2319b275636d6a9d21e2b6f5e26de3273b

---
+++ vc-simplify-citymodel/vc-simplify-citymodel.cpp
@@ -0,0 +1,59 @@
+// VirtualCity@Chalmers: vc-simplify-citymodel
+// Anders Logg 2019
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "CityModel.h"
+#include "CityModelGenerator.h"
+#include "CommandLine.h"
+#include "HeightMap.h"
+#include "JSON.h"
+#include "Parameters.h"
+#include "Polygon.h"
+
+using namespace VirtualCity;
+
+void Help()
+{
+  std::cerr << "Usage: vc-generate-citymodel Parameters.json" << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+  // Check command-line arguments
+  if (argc != 2)
+  {
+    Help();
+    return 1;
+  }
+
+  // Read parameters
+  Parameters parameters;
+  JSON::Read(parameters, argv[1]);
+  std::cout << parameters << std::endl;
+
+  // Get data directory (add trailing slash just in case)
+  const std::string dataDirectory = parameters.DataDirectory + "/";
+
+  // Read city model data
+  CityModel cityModel;
+  JSON::Read(cityModel, dataDirectory + "CityModel.json");
+  std::cout << cityModel << std::endl;
+
+  // Read height map data
+  HeightMap heightMap;
+  JSON::Read(heightMap, dataDirectory + "HeightMap.json");
+  std::cout << heightMap << std::endl;
+
+  // Generate city model
+  CityModelGenerator::SimplifyCityModel(cityModel, heightMap,
+                                        parameters.MinimalBuildingDistance);
+  std::cout << cityModel << std::endl;
+
+  // Write city model to file
+  JSON::Write(cityModel, dataDirectory + "SimplifiedCityModel.json");
+
+  return 0;
+}
