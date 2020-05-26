808adbae0207b80f3717ac5335994b55f3dd4e6c
Orfeas Eleftheriou <orfeas.eleftheriou@gmail.com>
2020-05-26 12:44:48 +0300

    Trying to use assimp importer

---
+++ dtcc-hello-world/dtcc-hello-world.cpp
@@ -0,0 +1,19 @@
+// vc-generate-heightmap
+// Anders Logg 2019
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <assimp/Importer.hpp>      
+#include <assimp/scene.h>           
+#include <assimp/postprocess.h>     
+
+int main(int argc, char *argv[])
+{
+  // Check command-line arguments
+  std::cout << "Hello World!"<<std::endl;
+
+  Assimp::Importer Importer;
+
+  return 0;
+}
