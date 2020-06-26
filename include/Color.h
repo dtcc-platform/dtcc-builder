027221c5b15fd76fa22dda3a6fbfa6794e497f4c
Dag <dwastberg@gmail.com>
2020-06-26 10:11:00 +0000

    Add alpha

--- include/Color.h
+++ include/Color.h
@@ -14,8 +14,10 @@ namespace DTCC
         uint8_t R{}; 
         uint8_t G{}; 
         uint8_t B{}; 
+        uint8_t A{}; 
 
-        Color(uint8_t r, uint8_t g,uint8_t b): R(r), G(g), B(b) {}
+        Color(uint8_t r, uint8_t g,uint8_t b): R(r), G(g), B(b), A(255) {}
+        Color(uint8_t r, uint8_t g,uint8_t b, uint8_t a): R(r), G(g), B(b), A(a) {}
     };
 
 } // namespace DTCC
