7fdb6ff4745027e5fe6475197f594a171794b12c
Dag <dwastberg@gmail.com>
2020-06-26 11:49:23 +0000

    Add convert to string

--- include/Color.h
+++ include/Color.h
@@ -4,21 +4,40 @@
 #ifndef DTCC_COLOR_H
 #define DTCC_COLOR_H
 
+#include "Logging.h"
+
 #include <cstdint>
 
 namespace DTCC
 {
-    class Color
-    {
-    public:
-        uint8_t R{}; 
-        uint8_t G{}; 
-        uint8_t B{}; 
-        uint8_t A{}; 
+class Color
+{
+public:
+  /// Red (R)
+  uint8_t R{};
 
-        Color(uint8_t r, uint8_t g,uint8_t b): R(r), G(g), B(b), A(255) {}
-        Color(uint8_t r, uint8_t g,uint8_t b, uint8_t a): R(r), G(g), B(b), A(a) {}
-    };
+  /// Green (G)
+  uint8_t G{};
+
+  /// Blue (B)
+  uint8_t B{};
+
+  /// Alpha (A)
+  uint8_t A{};
+
+  /// Empty constructor
+  Color() {}
+
+  Color(uint8_t r, uint8_t g, uint8_t b) : R(r), G(g), B(b), A(255) {}
+  Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a) : R(r), G(g), B(b), A(a) {}
+
+  // Convert to string (pretty-print)
+  std::string __str__() const
+  {
+    std::string s = "(" + str(R) + ", " + str(G) + ", " + str(B) + ", " + str(A) +")";
+    return s;
+  }
+};
 
 } // namespace DTCC
 #endif
