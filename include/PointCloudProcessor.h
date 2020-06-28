1e6724a196ca8685a384ec05bee955ee4b599da9
Dag <dwastberg@gmail.com>
2020-06-28 17:13:11 +0000

    Initial commit color pointclouds

---
+++ include/PointCloudProcessor.h
@@ -0,0 +1,42 @@
+// Copyright (C) 2020 Anders Logg
+// Licensed under the MIT License
+
+#ifndef DTCC_POINT_CLOUD_PROCESSOR_H
+#define DTCC_POINT_CLOUD_PROCESSOR_H
+
+#include "Color.h"
+#include "GeoRaster.h"
+#include "PointCloud.h"
+
+namespace DTCC
+{
+
+class PointCloudProcessor
+{
+public:
+  // Georaster is assumed to have at least 3 bands where 1,2,3 are R,G,B
+  // and in range 0-255
+  static void ColorFromImage(PointCloud &pointCloud, const GeoRaster &raster )  
+  {
+      double r,g,b;
+      pointCloud.Colors.clear();
+      
+      for (auto p: pointCloud.Points) {
+        Point2D p2d = Point2D(p.x,p.y);
+        r = raster(p2d,1);
+        g = raster(p2d,2);
+        b = raster(p2d,3);
+        r /= 255;
+        g /= 255;
+        b /= 255;
+        pointCloud.Colors.push_back(Color(r,g,b));
+      }
+
+  }
+
+};
+
+} // namespace DTCC
+
+
+#endif
