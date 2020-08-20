01a2542f9e8077562e5b370467205e108dee4a19
Dag <dwastberg@gmail.com>
2020-08-20 10:29:37 +0000

    updated Color class

--- include/Color.h
+++ include/Color.h
@@ -37,7 +37,7 @@ public:
   Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a) : R(r/255.0), G(g/255.0), B(b/255.0), A(a/255.0) {}
 
   // Convert to string (pretty-print)
-  std::string __str__() const
+  std::string __str__() const 
   {
     std::string s = "(" + str(R) + ", " + str(G) + ", " + str(B) + ", " + str(A) +")";
     return s;
