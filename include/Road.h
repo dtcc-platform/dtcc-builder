e54445461767b72ae4f0d283ca59d0e6c69ffb0a
Anton J Olsson <anton.j.olsson@bredband.net>
2020-10-28 19:35:18 +0000

    Add fields Code and Category to Road.h (TODO: Doxygen comments)

--- include/Road.h
+++ include/Road.h
@@ -11,29 +11,33 @@
 
 namespace DTCC
 {
-  class Road : public Printable
+class Road : public Printable
+{
+public:
+  int Code;
+
+  std::string Category;
+
+  /** Holds the road vertex positions. */
+  std::vector<Point2D> Vertices;
+
+  /** Holds the vertex indices indicating the road edges. */
+  std::vector<unsigned int> Edges;
+
+  /** Holds vectors of additional vertex values.  */
+  std::unordered_map<std::string, std::vector<double>> VertexValues;
+
+  /** Holds vectors of additional edge values. */
+  std::unordered_map<std::string, std::vector<double>> EdgeValues;
+
+  std::string __str__() const override
   {
-  public:
-    /** Holds the road vertex positions. */
-    std::vector<Point2D> Vertices;
-
-    /** Holds the vertex indices indicating the road edges. */
-    std::vector<unsigned int> Edges;
-
-    /** Holds vectors of additional vertex values.  */
-    std::unordered_map<std::string, std::vector<double>> VertexValues;
-
-    /** Holds vectors of additional edge values. */
-    std::unordered_map<std::string, std::vector<double>> EdgeValues;
-
-    std::string __str__() const override
-    {
-      return "Road with " +
-             str(Vertices.size()) + " vertices, " +
-             str(Edges.size() / 2) + " edges, " +
-             str(VertexValues.size()) + " vertex value arrays, and " +
-             str(EdgeValues.size()) + " edge value arrays";
-    }
+    return "Road with " + std::string("code ") + std::to_string(Code) +
+           std::string(", category ") + Category + ", " + str(Vertices.size()) +
+           " vertices, " + str(Edges.size() / 2) + " edges, " +
+           str(VertexValues.size()) + " vertex value arrays, and " +
+           str(EdgeValues.size()) + " edge value arrays";
+  }
   };
 } // namespace DTCC
 
