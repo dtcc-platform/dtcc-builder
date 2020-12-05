3c3704ce3f5a483f81c44122e4b8fa25c2479c65
root <root@compute.dtcc.chalmers.se>
2020-12-05 11:30:48 +0100

    Progress in triangulation. Almost done, but still WiP.

--- dtcc-sandbox/dtcc-sandbox.cpp
+++ dtcc-sandbox/dtcc-sandbox.cpp
@@ -9,6 +9,7 @@
 #include "Logging.h"
 #include <vtkCellArray.h>
 #include <vtkSmartPointer.h>
+#include <vtkTetra.h>
 #include <vtkTriangle.h>
 #include <vtkXMLUnstructuredGridReader.h>
 //#include <vtkDataSetMapper.h>
@@ -39,32 +40,71 @@ int main(int argc, char *argv[])
   // nlohmann::json json;
   // in.read_header(io::ignore_extra_column, "vendor", "size", "speed");
   // std::string vendor; int size; double speed;
-  Mesh3D mesh;
+  Mesh2D mesh2D;
+  // JSON::Read(mesh2D, dataDirectory + "Mesh2D.json");
+  JSON::Read(mesh2D, "/home/dtcc/core/build/Mesh2D.json");
+  std::cout << mesh2D.Cells[0] << std::endl;
+  std::cout << mesh2D.Cells[1] << std::endl;
+  std::cout << mesh2D.Cells.size() << std::endl;
+  std::cout << mesh2D.Vertices.size() << std::endl;
+  // std::cout<<mesh2D.Cells[0].size()<<std::endl;
+  // std::cout<<mesh2D.Vertices[0].size()<<std::endl;
 
-  CSV csv;
-  //csv.Read("test.csv", true);
-  CityJSON cityobj;
+  /*
+  Mesh3D mesh;
+  mesh.Vertices.push_back(Point3D(0, 0, 0));
+  mesh.Vertices.push_back(Point3D(1, 0, 0));
+  mesh.Vertices.push_back(Point3D(0, 1, 0));
+  mesh.Vertices.push_back(Point3D(0.5, 0.5, 1));
+  mesh.Cells.push_back(Simplex3D(0, 1, 2, 3));
+    std::cout<<mesh.Vertices[0].x<<std::endl;
+  */
+  // CSV csv;
+  // csv.Read("test.csv", true);
+  // CityJSON cityobj;
   // JSON::Read(cityobj,"test.json");
   // std::cout<<cityobj.CityObjects[0]<<std::endl;
 
-  std::string filename = "test";
+  std::string filename = "test.vtu";
 
   vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
-  points->InsertNextPoint(0, 0, 0);
-  points->InsertNextPoint(1, 0, 0);
-  points->InsertNextPoint(1, 1, 0);
-  //  points->InsertNextPoint(0, 1, 1);
 
-  vtkSmartPointer<vtkTriangle> tetra = vtkSmartPointer<vtkTriangle>::New();
+  for (uint i = 0; i < mesh2D.Vertices.size(); i++)
+  {
+    points->InsertNextPoint(mesh2D.Vertices[i].x, mesh2D.Vertices[i].y, 0);
+  }
 
-  tetra->GetPointIds()->SetId(0, 0);
-  tetra->GetPointIds()->SetId(1, 1);
-  tetra->GetPointIds()->SetId(2, 2);
-  //  tetra->GetPointIds()->SetId(3, 3);
+  vtkSmartPointer<vtkTriangle> triangle = vtkSmartPointer<vtkTriangle>::New();
+
+  vtkSmartPointer<vtkTriangle> triangle2 = vtkSmartPointer<vtkTriangle>::New();
+
+  triangle->GetPointIds()->SetId(0, mesh2D.Cells[0].v0);
+  triangle->GetPointIds()->SetId(1, mesh2D.Cells[0].v1);
+  triangle->GetPointIds()->SetId(2, mesh2D.Cells[0].v2);
+
+  triangle2->GetPointIds()->SetId(0, mesh2D.Cells[1].v0);
+  triangle2->GetPointIds()->SetId(1, mesh2D.Cells[1].v1);
+  triangle2->GetPointIds()->SetId(2, mesh2D.Cells[1].v2);
+
+  /*
+    for (uint i = 0; i<3;i++)
+    {
+    points->InsertNextPoint(mesh2D.Vertices[i].x, mesh2D.Vertices[i].y,0);
+    }
+  */
+  // Find first triangle
+  //   points->InsertNextPoint(mesh2D.Vertices[0].x, mesh2D.Vertices[0].y, 0);
+  ///   points->InsertNextPoint(mesh2D.Vertices[1].x, mesh2D.Vertices[1].y, 0);
+  //  points->InsertNextPoint(mesh2D.Vertices[2].x, mesh2D.Vertices[2].y, 0);
+
+  // points->InsertNextPoint(1, 0, 0);
+  // points->InsertNextPoint(1, 1, 0);
+  // points->InsertNextPoint(0, 1, 1);
 
   vtkSmartPointer<vtkCellArray> cellArray =
       vtkSmartPointer<vtkCellArray>::New();
-  cellArray->InsertNextCell(tetra);
+  cellArray->InsertNextCell(triangle);
+  cellArray->InsertNextCell(triangle2);
 
   vtkSmartPointer<vtkUnstructuredGrid> unstructuredGrid =
       vtkSmartPointer<vtkUnstructuredGrid>::New();
