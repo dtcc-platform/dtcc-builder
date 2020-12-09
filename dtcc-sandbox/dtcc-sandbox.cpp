fc72a1f4a94c8da2ba57ed1e42568239601414b5
Vasilis Naserentin <vasnas@chalmers.se>
2020-12-09 09:59:54 +0000

    Added WiP for Meshfield writter.

--- dtcc-sandbox/dtcc-sandbox.cpp
+++ dtcc-sandbox/dtcc-sandbox.cpp
@@ -1,66 +1,45 @@
-// dtcc-sandbox
-// Just a sandbox code for users to experiment with different API calls for
-// core Vasilis Naserentin 2019
-// Licensed under the MIT License
+#include <vtkCellArray.h>
+#include <vtkCellData.h>
+#include <vtkFloatArray.h>
+#include <vtkPointData.h>
+#include <vtkPoints.h>
+#include <vtkSmartPointer.h>
+#include <vtkStructuredGrid.h>
+#include <vtkXMLStructuredGridWriter.h>
 
-#include "CSV.h"
-#include "CityJSON.h"
-#include "JSON.h"
-#include "Logging.h"
-#include "VTK.h"
-
-#include <iostream>
-using namespace std;
-using namespace DTCC;
-
-void Help() { Error("Usage: dtcc-sandbox"); }
-
-int main(int argc, char *argv[])
+int main(int, char *[])
 {
-  if (argc > 4)
-  {
-    Help();
-    return 1;
-  }
-  // std::string varname = argv[3];
-  // std::string fileout = argv[2];
-  // std::string filein = argv[1];
-  // nlohmann::json json;
-  // in.read_header(io::ignore_extra_column, "vendor", "size", "speed");
-  // std::string vendor; int size; double speed;
-  Mesh2D mesh2D;
-  Mesh3D mesh3D;
-  Surface3D surface3D;
+  vtkSmartPointer<vtkStructuredGrid> structuredGrid =
+      vtkSmartPointer<vtkStructuredGrid>::New();
 
-  // JSON::Read(mesh2D, dataDirectory + "Mesh2D.json");
-  JSON::Read(mesh2D, "/home/dtcc/core/build/Mesh2D.json");
-  JSON::Read(mesh3D, "/home/dtcc/core/build/Mesh3D.json");
-  JSON::Read(surface3D, "/home/dtcc/core/build/SmoothedGroundMesh.json");
+  vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
 
-  std::cout << mesh2D.Cells[0] << std::endl;
-  std::cout << mesh2D.Cells[1] << std::endl;
-  std::cout << mesh2D.Cells.size() << std::endl;
-  std::cout << mesh2D.Vertices.size() << std::endl;
-  // std::cout<<mesh2D.Cells[0].size()<<std::endl;
-  // std::cout<<mesh2D.Vertices[0].size()<<std::endl;
+  for (int i = 0; i < 3; ++i)
+    for (int j = 0; j < 3; j++)
+      for (int k = 0; k < 3; k++)
+      {
+        {
+          {
+            points->InsertNextPoint(i, j, k);
+          }
+        }
+      }
 
-  /*
-  Mesh3D mesh;
-  mesh.Vertices.push_back(Point3D(0, 0, 0));
-  mesh.Vertices.push_back(Point3D(1, 0, 0));
-  mesh.Vertices.push_back(Point3D(0, 1, 0));
-  mesh.Vertices.push_back(Point3D(0.5, 0.5, 1));
-  mesh.Cells.push_back(Simplex3D(0, 1, 2, 3));
-    std::cout<<mesh.Vertices[0].x<<std::endl;
-  */
-  // CSV csv;
-  // csv.Read("test.csv", true);
-  // CityJSON cityobj;
-  // JSON::Read(cityobj,"test.json");
-  // std::cout<<cityobj.CityObjects[0]<<std::endl;
-  VTK::Write(surface3D, "surfaceTEST3D.vtu");
-  VTK::Write(mesh2D, "TEST2D.vtu");
-  VTK::Write(mesh3D, "TEST3D.vtu");
+  //
+  structuredGrid->SetDimensions(3, 3, 3);
+  structuredGrid->SetPoints(points);
 
-  //DTCC::CityJSON::Read("HI");
+  vtkFloatArray *scalars = vtkFloatArray::New();
+  for (int i = 0; i < 27; i++)
+    scalars->InsertTuple1(i, 1.0 * i / (27));
+  structuredGrid->GetPointData()->SetScalars(scalars);
+
+  // Write file
+  vtkSmartPointer<vtkXMLStructuredGridWriter> writer =
+      vtkSmartPointer<vtkXMLStructuredGridWriter>::New();
+  writer->SetFileName("output.vts");
+  writer->SetInputData(structuredGrid);
+  writer->Write();
+
+  return EXIT_SUCCESS;
 }
