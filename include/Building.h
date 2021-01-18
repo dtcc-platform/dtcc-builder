4be21ab68ed6eab6ddf110d8655e44c0a5bda68e
Anders Logg <logg@chalmers.se>
2021-01-18 09:21:54 +0100

    Add RoofPoints to Building + some cleanup, points not yet computed

--- include/Building.h
+++ include/Building.h
@@ -8,40 +8,50 @@
 #include <vector>
 
 #include "Logging.h"
+#include "Point.h"
 #include "Polygon.h"
 #include "Vector.h"
 
 namespace DTCC
 {
 
+/// Building represents a building defined by a footprint and a height.
+/// This means that a Building is currently an LOD1 representation.
 class Building : public Printable
 {
 public:
-  // Building UUID
-  std::string UUID;
+  /// Universally unique identifier of building
+  std::string UUID{};
 
-  // Shapefile ID
+  /// ID (index) of bulding when parsed from shapefile
   int SHPFileID{};
+
+  /// Footprint of building (a 2D polygon)
+  Polygon Footprint{};
+
+  /// Height of building relative to ground height
+  double Height{};
+
+  /// Height of ground at site of building
+  double GroundHeight{};
+
+  /// Array of roof points (from point cloud) inside building footprint
+  std::vector<Point3D> RoofPoints{};
+
+  /// Create empty building
+  Building() = default;
 
   // Uncomment for debugging
   // int debugID;
 
-  // Building footprint (polygon)
-  Polygon Footprint{};
-
-  // Building height (relative to ground)
-  double Height{};
-
-  // Ground height (absolute)
-  double GroundHeight{};
-
-  // Create empty building
-  Building() = default;
-
-  // Return minimum absolute height of building
+  /// Return minimum absolute height of building (equal to ground height).
+  ///
+  /// @return Minimum absolute height of building
   double MinHeight() const { return GroundHeight; }
 
-  // Return maximum absolute height of building
+  /// Return maximum absolute height of building
+  ///
+  /// @return Maximum absolute height of building
   double MaxHeight() const { return GroundHeight + Height; }
 
   /// Pretty-print
