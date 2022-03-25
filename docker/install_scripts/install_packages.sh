b3eb6d5c1875e388617291b3fa58f52a1f72b594
Dag Wästberg <dwastberg@gmail.com>
2022-03-25 09:35:22 +0000

    Docker file uses build scripts

---
+++ docker/install_scripts/install_packages.sh
@@ -0,0 +1,32 @@
+!#/bin/bash
+
+apt-get update && apt-get install -y \
+    locales \
+    sudo \
+	build-essential \
+	cmake\
+    nlohmann-json3-dev \
+    libshp-dev \
+    libpugixml-dev \
+    libproj-dev \
+    libtriangle-dev \
+    libnetcdf-c++4-dev \
+    libpng++-dev \
+    clang-format \
+    clang-tidy \
+    doxygen graphviz\
+    libgeotiff-dev\
+    automake\
+    colordiff\
+    libuuid1 \
+    fenics \
+    gdal-bin \
+    git \
+    libgdal-dev \
+    nano \
+    rsync \
+    wget \
+    moreutils \
+    unzip \
+    jq \
+    uuid-dev
