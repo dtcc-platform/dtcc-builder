722e9992379ad5d303ad7bc09f8cc789a9024e54
Dag Wästberg <dwastberg@gmail.com>
2022-06-10 15:41:18 +0200

    install pcl script

--- docker/install_scripts/install_pcl.sh
+++ docker/install_scripts/install_pcl.sh
@@ -4,3 +4,5 @@ git checkout tags/pcl-1.12.1 -b pcl-1.12.1
 mkdir build && cd build
 cmake -DBUILD_visualization=OFF -DBUILD_stereo=OFF -DBUILD_features=OFF -DBUILD_registration=OFF -DBUILD_io=OFF -DBUILD_tracking=off -DBUILD_ml=OFF -DBUILD_recognition=OFF -DWITH_CUDA=OFF ..
 make -j4 && make install
+cd ../..
+rm -rf pcl
