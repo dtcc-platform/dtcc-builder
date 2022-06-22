b63661d3e3ab434879e3af5d9df309cc23d236f2
Vasilis Naserentin <vasilis.naserentin@chalmers.se>
2022-06-22 11:11:09 +0000

    Uninstall script.

--- docker/Windows/dtcc-uninstall.bat
+++ docker/Windows/dtcc-uninstall.bat
@@ -1,2 +1,4 @@
-docker container rm dtcc -f
-docker image rm dtccimage -f
+#docker container rm dtcc -f
+#docker image rm dtccimage -f
+docker image rm -f dtcc-builder-image
+docker container rm -f dtcc-builder
