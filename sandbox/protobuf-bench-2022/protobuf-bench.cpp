84e5d8f736ef496df963339bde0f5077874aaaf0
Anders Logg <logg@chalmers.se>
2022-09-28 15:08:59 +0200

    Improve results for Protobuf benchmark by using flat arrays
    ============================================
    protobuf-bench
    ============================================
    Task              CPU mean  CPU total  Count
    --------------------------------------------
    Access DTCC FLAT  0.654986  0.654986   1
    Access DTCC NEST  0.63249   0.63249    1
    Access PROT FLAT  0.67117   0.67117    1
    Access PROT NEST  0.814313  0.814313   1
    Create DTCC FLAT  0.67271   0.67271    1
    Create DTCC NEST  0.648334  0.648334   1
    Create PROT FLAT  1.21061   1.21061    1
    Create PROT NEST  3.47892   3.47892    1
    ============================================

--- sandbox/protobuf-bench-2022/protobuf-bench.cpp
+++ sandbox/protobuf-bench-2022/protobuf-bench.cpp
@@ -14,7 +14,6 @@
 #include "Mesh.h"
 #include "Timer.h"
 
-// Compute volume of tetrahedron
 double TetrahedronVolume(const double x0[], const double x1[], const double x2[], const double x3[])
 {
   return std::abs(x0[0]*(x1[1]*x2[2] + x3[1]*x1[2] + x2[1]*x3[2]
@@ -27,33 +26,45 @@ double TetrahedronVolume(const double x0[], const double x1[], const double x2[]
 			 - x1[1]*x0[2] - x2[1]*x1[2] - x0[1]*x2[2])) / 6.0;
 }
 
-// Create N x N x N mesh of unit cube
-void DTCCCreate(DTCC::Mesh3D &mesh, size_t N)
+class Mesh3DProtoFlat : public PROT::Mesh3DFlat
 {
-  DTCC::Timer timer("DTCC create");
+public:
 
-  const size_t nx = N;
-  const size_t ny = N;
-  const size_t nz = N;
+  const double* Vertex(int32_t i) const
+  {
+    return &vertices()[3*i];
+  }
+
+};
+
+//--- DTCC NEST ----------------------------------------------------------------
+
+void CreateDTCCNEST(DTCC::Mesh3D &mesh, uint32_t N)
+{
+  DTCC::Timer timer("Create DTCC NEST");
+
+  const uint32_t nx = N;
+  const uint32_t ny = N;
+  const uint32_t nz = N;
 
   const double hx = 1.0 / static_cast<double>(nx);
   const double hy = 1.0 / static_cast<double>(ny);
   const double hz = 1.0 / static_cast<double>(nz);
 
-  const size_t numVertices = (N+1)*(N+1)*(N+1);
-  const size_t numCells = 6*N*N*N;
+  const uint32_t numVertices = (N+1)*(N+1)*(N+1);
+  const uint32_t numCells = 6*N*N*N;
 
   //--- BENCH --------------------------------------
   mesh.Vertices.resize(numVertices);
   mesh.Cells.resize(numCells);
   //------------------------------------------------
 
-  size_t k = 0;
-  for (size_t iz = 0; iz < nz + 1; iz++)
+  uint32_t k = 0;
+  for (uint32_t iz = 0; iz < nz + 1; iz++)
   {
-    for (size_t iy = 0; iy < ny + 1; iy++)
+    for (uint32_t iy = 0; iy < ny + 1; iy++)
     {
-      for (size_t ix = 0; ix < nx + 1; ix++)
+      for (uint32_t ix = 0; ix < nx + 1; ix++)
       {
 	const double x = ix*hx;
 	const double y = iy*hy;
@@ -67,20 +78,20 @@ void DTCCCreate(DTCC::Mesh3D &mesh, size_t N)
   }
 
   k = 0;
-  for (size_t iz = 0; iz < nz; iz++)
+  for (uint32_t iz = 0; iz < nz; iz++)
   {
-    for (size_t iy = 0; iy < ny; iy++)
+    for (uint32_t iy = 0; iy < ny; iy++)
     {
-      for (size_t ix = 0; ix < nx; ix++)
+      for (uint32_t ix = 0; ix < nx; ix++)
       {
-	const size_t v0 = iz*(nx + 1)*(ny + 1) + iy*(nx + 1) + ix;
-	const size_t v1 = v0 + 1;
-	const size_t v2 = v0 + (nx + 1);
-	const size_t v3 = v1 + (nx + 1);
-	const size_t v4 = v0 + (nx + 1)*(ny + 1);
-	const size_t v5 = v1 + (nx + 1)*(ny + 1);
-	const size_t v6 = v2 + (nx + 1)*(ny + 1);
-	const size_t v7 = v3 + (nx + 1)*(ny + 1);
+	const uint32_t v0 = iz*(nx + 1)*(ny + 1) + iy*(nx + 1) + ix;
+	const uint32_t v1 = v0 + 1;
+	const uint32_t v2 = v0 + (nx + 1);
+	const uint32_t v3 = v1 + (nx + 1);
+	const uint32_t v4 = v0 + (nx + 1)*(ny + 1);
+	const uint32_t v5 = v1 + (nx + 1)*(ny + 1);
+	const uint32_t v6 = v2 + (nx + 1)*(ny + 1);
+	const uint32_t v7 = v3 + (nx + 1)*(ny + 1);
 
 	//--- BENCH --------------------------------------
 	mesh.Cells[k++] = DTCC::Simplex3D{v0, v1, v3, v7};
@@ -95,10 +106,9 @@ void DTCCCreate(DTCC::Mesh3D &mesh, size_t N)
   }
 }
 
-// Compute volume of mesh
-void DTCCAccess(const DTCC::Mesh3D &mesh)
+void AccessDTCCNEST(const DTCC::Mesh3D &mesh)
 {
-  DTCC::Timer timer("DTCC access");
+  DTCC::Timer timer("Access DTCC NEST");
 
   double V{0.0};
 
@@ -121,36 +131,143 @@ void DTCCAccess(const DTCC::Mesh3D &mesh)
     V += TetrahedronVolume(x0, x1, x2, x3);
   }
 
-  std::cout << "DTCC Volume = " << V << std::endl;
+  std::cout << "DTCC NEST Volume = " << V << std::endl;
 }
 
-// Create N x N x N mesh of unit cube
-void PROTCreate(PROT::Mesh3D &mesh, size_t N)
-{
-  DTCC::Timer timer("PROT create");
+//--- DTCC FLAT ----------------------------------------------------------------
 
-  const size_t nx = N;
-  const size_t ny = N;
-  const size_t nz = N;
+void CreateDTCCFLAT(DTCC::Mesh3DFlat &mesh, uint32_t N)
+{
+  DTCC::Timer timer("Create DTCC FLAT");
+
+  const uint32_t nx = N;
+  const uint32_t ny = N;
+  const uint32_t nz = N;
 
   const double hx = 1.0 / static_cast<double>(nx);
   const double hy = 1.0 / static_cast<double>(ny);
   const double hz = 1.0 / static_cast<double>(nz);
 
-  const size_t numVertices = (N+1)*(N+1)*(N+1);
-  const size_t numCells = 6*N*N*N;
+  const uint32_t numVertices = (N+1)*(N+1)*(N+1);
+  const uint32_t numCells = 6*N*N*N;
+
+  //--- BENCH --------------------------------------
+  mesh.Vertices.resize(3*numVertices);
+  mesh.Cells.resize(4*numCells);
+  //------------------------------------------------
+
+  uint32_t k = 0;
+  for (uint32_t iz = 0; iz < nz + 1; iz++)
+  {
+    for (uint32_t iy = 0; iy < ny + 1; iy++)
+    {
+      for (uint32_t ix = 0; ix < nx + 1; ix++)
+      {
+	const double x = ix*hx;
+        const double y = iy*hy;
+        const double z = iz*hz;
+
+        //--- BENCH --------------------------------------
+        mesh.Vertices[k++] = x;
+        mesh.Vertices[k++] = y;
+        mesh.Vertices[k++] = z;
+        //------------------------------------------------
+      }
+    }
+  }
+
+  k = 0;
+  for (uint32_t iz = 0; iz < nz; iz++)
+  {
+    for (uint32_t iy = 0; iy < ny; iy++)
+    {
+      for (uint32_t ix = 0; ix < nx; ix++)
+      {
+        const uint32_t v0 = iz*(nx + 1)*(ny + 1) + iy*(nx + 1) + ix;
+        const uint32_t v1 = v0 + 1;
+        const uint32_t v2 = v0 + (nx + 1);
+        const uint32_t v3 = v1 + (nx + 1);
+        const uint32_t v4 = v0 + (nx + 1)*(ny + 1);
+        const uint32_t v5 = v1 + (nx + 1)*(ny + 1);
+        const uint32_t v6 = v2 + (nx + 1)*(ny + 1);
+        const uint32_t v7 = v3 + (nx + 1)*(ny + 1);
+
+        //--- BENCH --------------------------------------
+        mesh.Cells[k++] = v0; mesh.Cells[k++]= v1; mesh.Cells[k++] = v3; mesh.Cells[k++]= v7;
+        mesh.Cells[k++] = v0; mesh.Cells[k++]= v1; mesh.Cells[k++] = v7; mesh.Cells[k++]= v5;
+        mesh.Cells[k++] = v0; mesh.Cells[k++]= v5; mesh.Cells[k++] = v7; mesh.Cells[k++]= v4;
+        mesh.Cells[k++] = v0; mesh.Cells[k++]= v3; mesh.Cells[k++] = v2; mesh.Cells[k++]= v7;
+        mesh.Cells[k++] = v0; mesh.Cells[k++]= v6; mesh.Cells[k++] = v4; mesh.Cells[k++]= v7;
+        mesh.Cells[k++] = v0; mesh.Cells[k++]= v2; mesh.Cells[k++] = v6; mesh.Cells[k++]= v7;
+        //------------------------------------------------
+      }
+    }
+  }
+}
+
+void AccessDTCCFLAT(const DTCC::Mesh3DFlat &mesh)
+{
+  DTCC::Timer timer("Access DTCC FLAT");
+
+  double V{0.0};
+
+  const auto &v{mesh.Vertices};
+  const auto &c{mesh.Cells};
+
+  const uint32_t n = mesh.Cells.size();
+  for (uint32_t i = 0; i < n; i+= 4)
+  {
+    //--- BENCH --------------------------------------
+    const uint32_t *vv = &c[i];
+    const double *x0 = &v[3*vv[0]];
+    const double *x1 = &v[3*vv[1]];
+    const double *x2 = &v[3*vv[2]];
+    const double *x3 = &v[3*vv[3]];
+
+    //const auto v0{3*c[i]};
+    //const auto v1{3*c[i + 1]};
+    //const auto v2{3*c[i + 2]};
+    //const auto v3{3*c[i + 3]};
+    //const double x0[]{v[v0], v[v0 + 1], v[v0 + 2]};
+    //const double x1[]{v[v1], v[v1 + 1], v[v1 + 2]};
+    //const double x2[]{v[v2], v[v2 + 1], v[v2 + 2]};
+    //const double x3[]{v[v3], v[v3 + 1], v[v3 + 2]};
+    //------------------------------------------------
+
+    V += TetrahedronVolume(x0, x1, x2, x3);
+  }
+
+  std::cout << "DTCC FLAT Volume = " << V << std::endl;
+}
+
+//--- PROT NEST ----------------------------------------------------------------
+
+void CreatePROTNEST(PROT::Mesh3D &mesh, uint32_t N)
+{
+  DTCC::Timer timer("Create PROT NEST");
+
+  const uint32_t nx = N;
+  const uint32_t ny = N;
+  const uint32_t nz = N;
+
+  const double hx = 1.0 / static_cast<double>(nx);
+  const double hy = 1.0 / static_cast<double>(ny);
+  const double hz = 1.0 / static_cast<double>(nz);
+
+  const uint32_t numVertices = (N+1)*(N+1)*(N+1);
+  const uint32_t numCells = 6*N*N*N;
 
   //--- BENCH --------------------------------------
   //mesh.Vertices.resize(numVertices);
   //mesh.Cells.resize(numCells);
   //------------------------------------------------
 
-  size_t k = 0;
-  for (size_t iz = 0; iz < nz + 1; iz++)
+  uint32_t k = 0;
+  for (uint32_t iz = 0; iz < nz + 1; iz++)
   {
-    for (size_t iy = 0; iy < ny + 1; iy++)
+    for (uint32_t iy = 0; iy < ny + 1; iy++)
     {
-      for (size_t ix = 0; ix < nx + 1; ix++)
+      for (uint32_t ix = 0; ix < nx + 1; ix++)
       {
 	const double x = ix*hx;
 	const double y = iy*hy;
@@ -167,20 +284,20 @@ void PROTCreate(PROT::Mesh3D &mesh, size_t N)
   }
 
   k = 0;
-  for (size_t iz = 0; iz < nz; iz++)
+  for (uint32_t iz = 0; iz < nz; iz++)
   {
-    for (size_t iy = 0; iy < ny; iy++)
+    for (uint32_t iy = 0; iy < ny; iy++)
     {
-      for (size_t ix = 0; ix < nx; ix++)
+      for (uint32_t ix = 0; ix < nx; ix++)
       {
-	const size_t v0 = iz*(nx + 1)*(ny + 1) + iy*(nx + 1) + ix;
-	const size_t v1 = v0 + 1;
-	const size_t v2 = v0 + (nx + 1);
-	const size_t v3 = v1 + (nx + 1);
-	const size_t v4 = v0 + (nx + 1)*(ny + 1);
-	const size_t v5 = v1 + (nx + 1)*(ny + 1);
-	const size_t v6 = v2 + (nx + 1)*(ny + 1);
-	const size_t v7 = v3 + (nx + 1)*(ny + 1);
+	const uint32_t v0 = iz*(nx + 1)*(ny + 1) + iy*(nx + 1) + ix;
+	const uint32_t v1 = v0 + 1;
+	const uint32_t v2 = v0 + (nx + 1);
+	const uint32_t v3 = v1 + (nx + 1);
+	const uint32_t v4 = v0 + (nx + 1)*(ny + 1);
+	const uint32_t v5 = v1 + (nx + 1)*(ny + 1);
+	const uint32_t v6 = v2 + (nx + 1)*(ny + 1);
+	const uint32_t v7 = v3 + (nx + 1)*(ny + 1);
 
 	//--- BENCH --------------------------------------
 	auto* c0 = mesh.add_cells();
@@ -201,51 +318,185 @@ void PROTCreate(PROT::Mesh3D &mesh, size_t N)
   }
 }
 
-// Compute volume of mesh
-void PROTAccess(const PROT::Mesh3D &mesh)
+void AccessPROTNEST(const PROT::Mesh3D &mesh)
 {
-  DTCC::Timer timer("PROT access");
+  DTCC::Timer timer("Access PROT NEST");
 
   double V{0.0};
 
-  for (size_t i = 0; i < mesh.cells_size(); i++)
+  const auto &v{mesh.vertices()};
+  const auto &c{mesh.cells()};
+
+  int32_t n = mesh.cells_size();
+  for (int32_t i = 0; i < n; i++)
   {
     //--- BENCH --------------------------------------
-    const auto &c{mesh.cells(i)};
-    const auto &v0{mesh.vertices(c.v0())};
-    const auto &v1{mesh.vertices(c.v1())};
-    const auto &v2{mesh.vertices(c.v2())};
-    const auto &v3{mesh.vertices(c.v3())};
+    const auto &_c{c[i]};
+    const auto &v0{v[_c.v0()]};
+    const auto &v1{v[_c.v1()]};
+    const auto &v2{v[_c.v2()]};
+    const auto &v3{v[_c.v3()]};
 
     const double x0[]{v0.x(), v0.y(), v0.z()};
     const double x1[]{v1.x(), v1.y(), v1.z()};
     const double x2[]{v2.x(), v2.y(), v2.z()};
     const double x3[]{v3.x(), v3.y(), v3.z()};
+
+    //const auto &_c{c[i]};
+    //const auto &v0{v[_c.v0_]};
+    //const auto &v1{v[_c.v1_]};
+    //const auto &v2{v[_c.v2_]};
+    //const auto &v3{v[_c.v3_]};
+
+    //const double x0[]{v0.x_, v0.y_, v0.z_};
+    //const double x1[]{v1.x_, v1.y_, v1.z_};
+    //const double x2[]{v2.x_, v2.y_, v2.z_};
+    //const double x3[]{v3.x_, v3.y_, v3.z_};
     //------------------------------------------------
 
     V += TetrahedronVolume(x0, x1, x2, x3);
   }
 
-  std::cout << "PROT Volume = " << V << std::endl;
+  std::cout << "PROT NEST Volume = " << V << std::endl;
+}
+
+//--- PROT FLAT  ---------------------------------------------------------------
+
+void CreatePROTFLAT(PROT::Mesh3DFlat &mesh, uint32_t N)
+{
+  DTCC::Timer timer("Create PROT FLAT");
+
+  const uint32_t nx = N;
+  const uint32_t ny = N;
+  const uint32_t nz = N;
+
+  const double hx = 1.0 / static_cast<double>(nx);
+  const double hy = 1.0 / static_cast<double>(ny);
+  const double hz = 1.0 / static_cast<double>(nz);
+
+  const uint32_t numVertices = (N+1)*(N+1)*(N+1);
+  const uint32_t numCells = 6*N*N*N;
+
+  //--- BENCH --------------------------------------
+  //mesh.Vertices.resize(numVertices);
+  //mesh.Cells.resize(numCells);
+  //------------------------------------------------
+
+  uint32_t k = 0;
+  for (uint32_t iz = 0; iz < nz + 1; iz++)
+  {
+    for (uint32_t iy = 0; iy < ny + 1; iy++)
+    {
+      for (uint32_t ix = 0; ix < nx + 1; ix++)
+      {
+	const double x = ix*hx;
+	const double y = iy*hy;
+	const double z = iz*hz;
+
+	//--- BENCH --------------------------------------
+	mesh.add_vertices(x);
+	mesh.add_vertices(y);
+	mesh.add_vertices(z);
+	//------------------------------------------------
+      }
+    }
+  }
+
+  k = 0;
+  for (uint32_t iz = 0; iz < nz; iz++)
+  {
+    for (uint32_t iy = 0; iy < ny; iy++)
+    {
+      for (uint32_t ix = 0; ix < nx; ix++)
+      {
+	const uint32_t v0 = iz*(nx + 1)*(ny + 1) + iy*(nx + 1) + ix;
+	const uint32_t v1 = v0 + 1;
+	const uint32_t v2 = v0 + (nx + 1);
+	const uint32_t v3 = v1 + (nx + 1);
+	const uint32_t v4 = v0 + (nx + 1)*(ny + 1);
+	const uint32_t v5 = v1 + (nx + 1)*(ny + 1);
+	const uint32_t v6 = v2 + (nx + 1)*(ny + 1);
+	const uint32_t v7 = v3 + (nx + 1)*(ny + 1);
+
+	//--- BENCH --------------------------------------
+	mesh.add_cells(v0); mesh.add_cells(v1); mesh.add_cells(v3); mesh.add_cells(v7);
+	mesh.add_cells(v0); mesh.add_cells(v1); mesh.add_cells(v7); mesh.add_cells(v5);
+	mesh.add_cells(v0); mesh.add_cells(v5); mesh.add_cells(v7); mesh.add_cells(v4);
+	mesh.add_cells(v0); mesh.add_cells(v3); mesh.add_cells(v2); mesh.add_cells(v7);
+	mesh.add_cells(v0); mesh.add_cells(v6); mesh.add_cells(v4); mesh.add_cells(v7);
+        mesh.add_cells(v0); mesh.add_cells(v2); mesh.add_cells(v6); mesh.add_cells(v7);
+	//------------------------------------------------
+      }
+    }
+  }
+}
+
+void AccessPROTFLAT(const PROT::Mesh3DFlat &mesh)
+{
+  DTCC::Timer timer("Access PROT FLAT");
+
+  double V{0.0};
+
+  const auto &v{mesh.vertices()};
+  const auto &c{mesh.cells()};
+
+  const int32_t n = mesh.cells_size();
+  for (int32_t i = 0; i < n; i+= 4)
+  {
+    //--- BENCH --------------------------------------
+    const uint32_t *vv = &c[i];
+    const double *x0 = &v[3*vv[0]];
+    const double *x1 = &v[3*vv[1]];
+    const double *x2 = &v[3*vv[2]];
+    const double *x3 = &v[3*vv[3]];
+
+    //const auto v0{3*c[i]};
+    //const auto v1{3*c[i + 1]};
+    //const auto v2{3*c[i + 2]};
+    //const auto v3{3*c[i + 3]};
+    //const double x0[]{v[v0], v[v0 + 1], v[v0 + 2]};
+    //const double x1[]{v[v1], v[v1 + 1], v[v1 + 2]};
+    //const double x2[]{v[v2], v[v2 + 1], v[v2 + 2]};
+    //const double x3[]{v[v3], v[v3 + 1], v[v3 + 2]};
+    //------------------------------------------------
+
+    V += TetrahedronVolume(x0, x1, x2, x3);
+  }
+
+  std::cout << "PROT FLAT Volume = " << V << std::endl;
 }
 
 int main()
 {
   // Size of mesh (unit cube N x N x N)
-  const size_t N{256};
+  const uint32_t N{256};
 
-  // Run benchmarks for DTCC data structures
+  // DTCC NEST
   {
     DTCC::Mesh3D mesh{};
-    DTCCCreate(mesh, N);
-    DTCCAccess(mesh);
+    CreateDTCCNEST(mesh, N);
+    AccessDTCCNEST(mesh);
   }
 
-  // Run benchmarks for Protobuf data structures
+  // DTCC FLAT
+  {
+    DTCC::Mesh3DFlat mesh{};
+    CreateDTCCFLAT(mesh, N);
+    AccessDTCCFLAT(mesh);
+  }
+
+  // PROT NEST
   {
     PROT::Mesh3D mesh{};
-    PROTCreate(mesh, N);
-    PROTAccess(mesh);
+    CreatePROTNEST(mesh, N);
+    AccessPROTNEST(mesh);
+  }
+
+  // PROT FLAT
+  {
+    PROT::Mesh3DFlat mesh{};
+    CreatePROTFLAT(mesh, N);
+    AccessPROTFLAT(mesh);
   }
 
   // Report timings
