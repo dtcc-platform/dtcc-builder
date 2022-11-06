f1b3687da0ec5ca64e5b2d32979ce328d4d81efe
Dag Wästberg <dwastberg@gmail.com>
2022-11-06 13:33:16 +0000

    add all meshing functions

--- pybuilder/pybuilder/Meshing.py
+++ pybuilder/pybuilder/Meshing.py
@@ -15,7 +15,7 @@ def generateMesh2D(
 def generateMesh3D(
     mesh: _pybuilder.Mesh2D, domain_height, mesh_resolution
 ) -> _pybuilder.Mesh3D:
-    pass
+    return _pybuilder.GenerateMesh3D(mesh, domain_height, mesh_resolution)
 
 
 def smoothMesh3D(
@@ -25,14 +25,13 @@ def smoothMesh3D(
     top_height: float,
     fix_buildings: bool,
 ):
-    return _pybuilder.SmoothMesh3D(mesh,city_model,dem,top_height,fix_buildings)
-    
+    return _pybuilder.SmoothMesh3D(mesh, city_model, dem, top_height, fix_buildings)
 
 
 def generateSurface3D(
     city_model: _pybuilder.CityModel, dtm: _pybuilder.GridField2D, resolution
 ):
-    pass
+    return _pybuilder.GenerateSurfaces3D(city_model, dtm, resolution)
 
 
 def trimMesh3D(
@@ -41,16 +40,16 @@ def trimMesh3D(
     city_model: _pybuilder.CityModel,
     num_layers,
 ) -> _pybuilder.Mesh3D:
-    pass
+    return _pybuilder.TrimMesh3D(mesh, mesh2D, city_model, num_layers)
 
 
 def extractBoundary3D(mesh: _pybuilder.Mesh3D) -> _pybuilder.Surface3D:
-    pass
+    return _pybuilder.ExtractBoundary3D(mesh)
 
 
 def extractOpenSurface3D(boundary: _pybuilder.Surface3D) -> _pybuilder.Surface3D:
-    pass
+    return _pybuilder.ExtractOpenSurface3D(boundary)
 
 
 def mergeSurface3D(surfaces: List[_pybuilder.Surface3D]) -> _pybuilder.Surface3D:
-    pass
+    return _pybuilder.MergeSurface3D(surfaces)
