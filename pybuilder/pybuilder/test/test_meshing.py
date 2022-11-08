3ae7de279c37ee439e42d4cbe3d407e966d17455
Dag Wästberg <dwastberg@gmail.com>
2022-11-08 09:01:47 +0000

    clean before meshing

--- pybuilder/pybuilder/test/test_meshing.py
+++ pybuilder/pybuilder/test/test_meshing.py
@@ -14,28 +14,37 @@ import Meshing
 
 
 data_dir = (Path(__file__).parent / "../../../unittests/data").resolve()
-p = Parameters.loadParameters()
+p = Parameters.loadParameters(data_dir / "MinimalCase" / "Parameters.json")
+
 
 class TestMeshing(unittest.TestCase):
     def setUp(self):
         shp_file = data_dir / "MinimalCase" / "PropertyMap.shp"
 
-        self.cm_bounds = CityModel.buildingBounds(shp_file,10)
-        self.pc_bounds = PointCloud.getLasBounds(data_dir / "MinimalCase" )
+        self.cm_bounds = CityModel.buildingBounds(shp_file, 10)
+        self.pc_bounds = PointCloud.getLasBounds(data_dir / "MinimalCase")
         print(self.cm_bounds)
         print(self.pc_bounds)
-        self.bounds = boundsIntersect(self.cm_bounds,self.pc_bounds)
+        self.bounds = boundsIntersect(self.cm_bounds, self.pc_bounds)
 
         self.cm = CityModel.generateCityModel(shp_file, p)
-        self.cm = CityModel.setOrigin(self.cm,(self.bounds[0],self.bounds[1]))
-        
+        self.cm = CityModel.setOrigin(self.cm, (self.bounds[0], self.bounds[1]))
+
         self.pc = PointCloud.readLasFiles(data_dir / "MinimalCase" / "pointcloud.las")
-        self.pc = PointCloud.setOrigin(self.pc,(self.bounds[0],self.bounds[1]))
+        self.pc = PointCloud.setOrigin(self.pc, (self.bounds[0], self.bounds[1]))
+
+        self.cm = CityModel.extractBuildingPoints(self.cm, self.pc, 1.0, 2.0)
 
         self.dtm = ElevationModel.generateElevationModel(self.pc, 0.5, [2, 9])
+        self.cm = CityModel.computeBuildingHeights(self.cm, self.dtm, 0.9, 0.95)
 
+        self.cm = CityModel.cleanCityModel(self.cm, p["MinVertexDistance"])
 
+    def test_buildings_have_height(self):
+        self.assertEqual(self.cm.buildings[0].height, 5)
 
     def test_mesh2D(self):
-        mesh = Meshing.generateMesh2D(self.cm, ElevationModel.getBounds(self.dtm), p["MeshResolution"] )
+        mesh = Meshing.generateMesh2D(
+            self.cm, ElevationModel.getBounds(self.dtm), p["MeshResolution"]
+        )
         self.assertIsInstance(mesh, _pybuilder.Mesh2D)
