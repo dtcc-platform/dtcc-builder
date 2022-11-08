81fb22309d842ac29662fbe517f2475e34fb4193
Dag Wästberg <dwastberg@gmail.com>
2022-11-08 20:43:12 +0000

    point cloud class

--- pybuilder/pybuilder/PointCloud.py
+++ pybuilder/pybuilder/PointCloud.py
@@ -4,24 +4,7 @@ import numpy
 from typing import List, Tuple
 
 
-def read_las_files(las_path, extra_data=True):
-    las_path = str(las_path)
-
-    # print(f"loading las from {las_path}")
-    if os.path.isdir(las_path):
-        pc = _pybuilder.LASReadDirectory(las_path, extra_data)
-    elif os.path.isfile(las_path):
-        pc = _pybuilder.LASReadFile(las_path, extra_data)
-    return pc
-
-
-def set_origin(
-    point_cloud: _pybuilder.PointCloud, origin: Tuple[float, float]
-) -> _pybuilder.PointCloud:
-    return _pybuilder.SetPointCloudOrigin(point_cloud, origin)
-
-
-def get_las_bounds(las_path):
+def calc_las_bounds(las_path):
     las_path = str(las_path)
     if not os.path.isdir(las_path):
         print(f"{las_path} is not a directory")
@@ -30,36 +13,49 @@ def get_las_bounds(las_path):
     return bbox
 
 
-def global_outlier_remover(point_cloud, outlierMargin):
-    point_cloud = _pybuilder.GlobalOutlierRemover(point_cloud, outlierMargin)
-    return point_cloud
+class PointCloud:
+    def __init__(self, las_path=None):
+        self._builder_pc = None
+        self.origin = (0, 0)
+        self.bounds = None
+        if las_path is not None:
+            self.read_las_files(las_path)
 
+    def read_las_files(self, las_path, extra_data=True):
+        las_path = str(las_path)
 
-def vegetation_filter(point_cloud):
-    point_cloud = _pybuilder.VegetationFilter(point_cloud)
-    return point_cloud
+        # print(f"loading las from {las_path}")
+        if os.path.isdir(las_path):
+            pc = _pybuilder.LASReadDirectory(las_path, extra_data)
+        elif os.path.isfile(las_path):
+            pc = _pybuilder.LASReadFile(las_path, extra_data)
+        self._builder_pc = pc
 
+    def set_origin(self, origin: Tuple[float, float]):
+        self.origin = origin
+        self._builder_pc = _pybuilder.SetPointCloudOrigin(self._builder_pc, origin)
 
-def pointcloud_to_numpy(point_cloud):
-    pts = point_cloud.points
-    pts = [[p.x, p.y, p.z] for p in pts]
-    return numpy.array(pts)
+    def remove_global_outliers(self, outlier_margin):
+        self._builder_pc = _pybuilder.GlobalOutlierRemover(
+            self._builder_pc, outlier_margin
+        )
 
+    def vegetation_filter(self):
+        self._builder_pc = _pybuilder.VegetationFilter(self._builder_pc)
 
-# def setOrigin(point_cloud, origin):
-#     return _pybuilder.SetPointCloudOrigin(point_cloud, origin)
+    def points_as_numpy(self):
+        pts = self._builder_pc.points
+        pts = [[p.x, p.y, p.z] for p in pts]
+        return numpy.array(pts)
 
+    def get_bounds(self):
+        pts = pointcloud_to_numpy(self._builder_pc)
+        x_min, y_min, z_min = pts.min(axis=0)
+        x_max, y_max, z_max = pts.max(axis=0)
+        return (x_min, y_min, x_max, y_max)
 
-def get_bounds(point_cloud):
-    pts = pointcloud_to_numpy(point_cloud)
-    x_min, y_min, z_min = pts.min(axis=0)
-    x_max, y_max, z_max = pts.max(axis=0)
-    return (x_min, y_min, x_max, y_max)
+    def init_with_protobuf(self, protobuf_string: str):
+        self._builder_pc = _pybuilder.loadPointCloudProtobuf(protobuf_string)
 
-
-def load_protobuf(protobuf_string: str) -> _pybuilder.PointCloud:
-    return _pybuilder.loadPointCloudProtobuf(protobuf_string)
-
-
-def to_protobuf(pc: _pybuilder.PointCloud) -> str:
-    return _pybuilder.convertPointCloudToProtobuf(pc)
+    def to_protobuf(self) -> str:
+        return _pybuilder.convertPointCloudToProtobuf(self._builder_pc)
