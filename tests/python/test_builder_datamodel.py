9abeac3a4d83ced967be4821a82386b2b83b2e8c
Anders Logg <logg@chalmers.se>
2023-06-29 10:40:08 +0200

    Fix unit tests

--- tests/python/test_builder_datamodel.py
+++ tests/python/test_builder_datamodel.py
@@ -11,7 +11,7 @@ data_dir = (Path(__file__).parent / "../data").resolve()
 class TestBuilderPointCloud(unittest.TestCase):
     def test_convert_pointcloud(self):
         pc = io.load_pointcloud(data_dir / "MinimalCase" / "pointcloud.las")
-        builder_pc = builder.builder_datamodel.create_builder_pointcloud(pc)
+        builder_pc = builder.model.create_builder_pointcloud(pc)
         self.assertEqual(len(builder_pc.points), len(pc.points))
         self.assertEqual(len(builder_pc.points), 8148)
 
@@ -19,6 +19,6 @@ class TestBuilderPointCloud(unittest.TestCase):
 class TestBuilderCity(unittest.TestCase):
     def test_convert_city(self):
         city = io.load_city(data_dir / "MinimalCase" / "PropertyMap.shp")
-        builder_city = builder.builder_datamodel.create_builder_city(city)
+        builder_city = builder.model.create_builder_city(city)
         self.assertEqual(len(builder_city.buildings), len(city.buildings))
         self.assertEqual(len(builder_city.buildings), 5)
