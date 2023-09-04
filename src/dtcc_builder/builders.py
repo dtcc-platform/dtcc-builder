14c38ef1807cc73ea487780e675c95c1d9749adf
Vasilis Naserentin <125367195+vbassn@users.noreply.github.com>
2023-09-04 09:51:42 +0300

    Testing dosctrings formatting on platform.dtcc.chalmers.se

--- src/dtcc_builder/builders.py
+++ src/dtcc_builder/builders.py
@@ -322,11 +322,9 @@ def build_volume_mesh(
 
 def build(parameters: dict = None) -> None:
     """
-    Build city and city meshes.
-
-    This function reads data from the specified data directory
-    and builds a city and its corresponding meshes. The same
-    thing can be accomplished by calling the individual build_*
+    Build city and city meshes (temp). This function reads data from the 
+    specified data directory and builds a city and its corresponding meshes. 
+    The same thing can be accomplished by calling the individual build_*
     functions, but this function is provided as a convenience
     and takes care of loading and saving data to files.
 
