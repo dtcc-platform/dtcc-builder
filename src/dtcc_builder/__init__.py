75dbebad101714c4b71355b7ba729506e38b190e
Anders Logg <logg@chalmers.se>
2023-09-04 11:48:45 +0200

    Expose compute_domain_bounds

--- src/dtcc_builder/__init__.py
+++ src/dtcc_builder/__init__.py
@@ -3,7 +3,13 @@ from . import builders
 from . import parameters
 from . import city_methods
 
-from .builders import build, build_city, build_mesh, build_volume_mesh
+from .builders import (
+    compute_domain_bounds,
+    build,
+    build_city,
+    build_mesh,
+    build_volume_mesh,
+)
 
 # Add model extensions
 from dtcc_model import City, PointCloud
@@ -11,4 +17,10 @@ from dtcc_model import City, PointCloud
 City.add_methods(city_methods.compute_building_points, "compute_building_points")
 City.add_methods(city_methods.compute_building_heights, "compute_building_heights")
 
-__all__ = ["build", "build_city", "build_mesh", "build_volume_mesh"]
+__all__ = [
+    "compute_domain_bounds",
+    "build",
+    "build_city",
+    "build_mesh",
+    "build_volume_mesh",
+]
