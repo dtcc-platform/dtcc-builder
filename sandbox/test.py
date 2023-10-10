1a7d8b16a8f1afe74692dd7588fec4a4d42b7452
George Spaias <auth-georspai@develop.dtcc>
2023-10-10 12:06:40 +0200

    Added sandbox script to run builder  at different resolutions

---
+++ sandbox/test.py
@@ -0,0 +1,42 @@
+import dtcc_model as model
+import dtcc_io as io
+import dtcc_builder as builder
+
+
+
+def main():
+    # Get parameters
+    p = builder.parameters.default()
+    
+    p["auto_domain"] = True 
+    #[102000.0, 102999.99] x [6213004.15744457, 6213999.99]
+    p["x_min"] = 102000.0
+    p["x_max"] = 102000.0 + 100 
+    
+    p["y_min"] = 6213004.15744457
+    p["y_max"] = 6213004.15744457 + 100
+    buildings_path = "/home/auth-georspai/dtcc-builder/data/HelsingborgResidential2022/PropertyMap.shp"
+    pointcloud_path = "/home/auth-georspai/dtcc-builder/data/HelsingborgResidential2022"
+    
+    origin, bounds = builder.calculate_bounds(buildings_path, pointcloud_path, p)
+    print(bounds)
+
+       # Load city
+    city = io.load_city(
+        buildings_path,
+        uuid_field=p["uuid_field"],
+        height_field=p["height_field"],
+        bounds=bounds,
+    )
+
+    # Load point cloud
+    point_cloud = io.load_pointcloud(pointcloud_path, bounds=bounds)
+
+    # Build city
+    city = builder.build_city(city, point_cloud, bounds, p)
+
+
+    
+
+if __name__== "__main__":
+    main()
