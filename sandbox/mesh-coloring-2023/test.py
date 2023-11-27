312d362967bc556370026c48b9e872ee73f9b870
Anders Logg <logg@chalmers.se>
2023-11-27 15:45:24 +0100

    Check colors for neighboring faces

--- sandbox/mesh-coloring-2023/test.py
+++ sandbox/mesh-coloring-2023/test.py
@@ -9,7 +9,9 @@ import matplotlib.tri as tri
 
 # Test mesh from ansys-test-case-2023 sandbox (run first)
 mesh = load_mesh("../ansys-test-case-2023/output/surface_mesh_tc_1.pb")
-mesh.view()
+num_vertices = len(mesh.vertices)
+num_faces = len(mesh.faces)
+#mesh.view()
 
 def ideal_layer_height(area):
     # Compute ideal layer height for regular tetrahedron
@@ -17,14 +19,28 @@ def ideal_layer_height(area):
     h = c*np.sqrt(area)
     return h
 
+def check_neighbors(colors, ff):
+    # Check coloring of neighboring faces
+    max_diff = 0
+    num_big_diffs = 0
+    for i, faces in enumerate(ff):
+        diffs = [abs(colors[i] - colors[j]) for j in faces]
+        big_diffs = [d for d in diffs if d > 1]
+        max_diff = max(max_diff, max(diffs))
+        if len(big_diffs) > 0:
+            #print(i, diffs)
+            num_big_diffs += 1
+    print("Max diff:", max_diff)
+    print(f"Num big diffs: {num_big_diffs} / {num_faces} ({100*num_big_diffs/num_faces:.2f}%)")
+
 # Compute mesh sizes
-areas = []
-for f in mesh.faces:
-    v = [mesh.vertices[i][:2] for i in f]
+areas = np.zeros(num_faces)
+for i, f in enumerate(mesh.faces):
+    v = [mesh.vertices[j][:2] for j in f]
     a = v[1] - v[0]
     b = v[2] - v[0]
     A = 0.5*abs(np.cross(a, b))
-    areas.append(A)
+    areas[i] = A
 
 # Compute ideal layer heights for smallest and largest mesh sizes
 _min_height = ideal_layer_height(np.min(areas))
@@ -44,14 +60,29 @@ print("Adjusted max layer height:", layer_heights[-1])
 print("Layer heights:", layer_heights)
 
 # Assign layer heights to mesh (closest by quotient)
-colors = np.zeros(len(areas), dtype=np.int32)
+colors = np.zeros(num_faces, dtype=np.int32)
 for i in range(len(areas)):
     h = ideal_layer_height(areas[i])
     d = np.abs(np.log(h / layer_heights))
     color = np.argmin(d)
     colors[i] = color
 
+# Build mapping from vertices to faces
+vf = [set() for i in range(num_vertices)]
+for i, f in enumerate(mesh.faces):
+    for j in f:
+        vf[j].add(i)
+
+# Build mapping from faces to faces
+ff = [set() for i in range(num_faces)]
+for i, f in enumerate(mesh.faces):
+    for j in f:
+        ff[i] |= vf[j]
+
+# Check coloring of neighboring faces
+check_neighbors(colors, ff)
+
 # Plot mesh colors
 _mesh = tri.Triangulation(mesh.vertices[:, 0], mesh.vertices[:, 1], mesh.faces)
 plt.tripcolor(_mesh, facecolors=colors, edgecolors="k")
-plt.show()
+#plt.show()
